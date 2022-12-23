2ebb2138f38a200a1603ce44ab0d24786974add0
MUJL <1219757241@qq.com>
2022-12-23 11:21:18 +0800

    修改入口main

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import tkinter as tk
+from tkinter import Tk
 
 import global_manager as gm
 from gui.login_page import LoginPage
 from database import db
 
-gm.root = tk.Tk()
+gm.root = Tk()
 LoginPage()
 gm.root.mainloop()
 print(gm.user['name'], '退出')
